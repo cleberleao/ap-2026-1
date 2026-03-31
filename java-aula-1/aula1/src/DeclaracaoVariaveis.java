88eb80d885d2a56cf168f08be8e396aedff92b07
cleberleao <cleberleao_14@hotmail.com>
2026-03-31 20:53:46 -0300

    primeiros passos em java

---
+++ java-aula-1/aula1/src/DeclaracaoVariaveis.java
@@ -0,0 +1,2 @@
+public class DeclaracaoVariaveis {
+}
---
+++ java-aula-1/aula1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello and welcome!");
+
+        String nome = "Cleber";
+        int idade = 45;
+        System.out.println("Meu nome é: "+ nome + "\nMinha idade é: " + idade);
+
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("i = " + i);
+//        }
+    }
+}
