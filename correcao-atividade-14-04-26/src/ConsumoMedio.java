f6694f749f96e7643a1e45cd8c04c3bc8164a851
cleberleao <cleberleao_14@hotmail.com>
2026-04-14 19:50:35 -0300

    atividade 1 corrifida

--- correcao-atividade-14-04-26/src/ConsumoMedio.java
+++ correcao-atividade-14-04-26/src/ConsumoMedio.java
@@ -1,2 +1,21 @@
+//Escrever um algoritmo para determinar o consumo médio de um automóvel sendo
+//fornecida a distância total percorrida pelo automóvel e o total de combustível gasto.
+
+import java.util.Scanner;
+
 public class ConsumoMedio {
+    public static void main(String[] args) {
+        float distanciaTotal, totalComustivelGasto, resultado;
+        Scanner lerDados = new Scanner(System.in);
+
+        System.out.printf("Digite a distancia percorrida: ");
+        distanciaTotal = lerDados.nextFloat();
+        System.out.printf("Digite o consumo total de combustivel: ");
+        totalComustivelGasto = lerDados.nextFloat();
+
+        resultado = distanciaTotal/totalComustivelGasto;
+
+        System.out.println("O consumo médio do veículo é: " + resultado + "Km/l");
+
+    }
 }
